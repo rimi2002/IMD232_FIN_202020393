dcddda8fc2f7ff7fcb920ede8f10d363821a9e09
rimi2002 <144625222+rimi2002@users.noreply.github.com>
2023-12-18 00:31:52 +0900

    12;31

--- script/Sketch.js
+++ script/Sketch.js
@@ -1,9 +1,16 @@
+let backgroundMusic;
+
+function preload() {
+  // 음악 파일 로드
+  backgroundMusic = loadSound('assets/music.mp3');
+}
+
 function setup() {
   // 캔버스 생성
-  createCanvas(800, 800, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 
   // 원근 투영 설정
-  ortho(-width / 2, width / 2, -height / 2, height / 2, -5000, 5000);
+  ortho(-width / 2, width / 2, -height / 2, height / 2, -4000, 4000);
 }
 
 function draw() {
@@ -11,14 +18,14 @@ function draw() {
   background(0);
 
   // 크기 조절 변수
-  let scl = 100;
+  let scl = 90;
 
   // 마우스로 화면 회전 가능하도록 설정
   orbitControl();
 
   // 파라미터 공간을 순회하면서 3D 모양 그리기
-  for (let v = 0; v < 1; v += 1 / 120) {
-    for (let u = 0; u < 1; u += 1 / 120) {
+  for (let v = 0; v < 1; v += 1 / 100) {
+    for (let u = 0; u < 1; u += 1 / 100) {
       // 파라미터를 이용하여 3D 모양 계산
       let x = (1 + sin(1 * PI * u) * sin(1 * PI * v)) * sin(4 * PI * v);
       let y = (1 + sin(1 * PI * u) * sin(1 * PI * v)) * cos(4 * PI * v);
@@ -32,8 +39,22 @@ function draw() {
       // 변환 후 상자 그리기
       push();
       translate(x, y, z);
+
+      // 색상 랜덤으로 설정
+      let r = random(0);
+      let g = random(100);
+      let b = random(255);
+      let a = 100;
+      fill(r, g, b, a);
+      stroke('#D6D7E2');
+
       box(12);
       pop();
     }
   }
+}
+
+// 윈도우 크기가 변경될 때 캔버스 크기도 조절
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
